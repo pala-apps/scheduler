a7373d85fa26ab627bebe611534217ae8563bd99
Jay Chetty <jaypaulchetty@gmail.com>
2016-11-14 12:19:48 +0000

    add react

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -13,6 +13,8 @@
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
 
+import React from 'react'
+
 // Import local files
 //
 // Local files can be imported directly using relative
