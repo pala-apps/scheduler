9528370fb289dd92f33acc0cf0e33d6466aca6f7
Jay Chetty <jaypaulchetty@gmail.com>
2016-11-14 17:05:56 +0000

    some tweaks

--- web/static/js/reducers/index.js
+++ web/static/js/reducers/index.js
@@ -8,8 +8,8 @@ const intitalState = {
   },
   positions:[
     {id: 1, name: "JS Teacher", start:"2016-11-14 00:00", end:"2016-12-14 00:00"},
-    {id: 2, name: "JS Assist One", start:"2016-11-14 00:00", end:"2016-11-21 00:00"},
-    {id: 3, name: "JS Assist Two", start:"2016-11-14 00:00", end:"2016-12-14 00:00"},
+    {id: 2, name: "JS Assist One", start:"2016-11-14 00:00", end:"2016-12-14 00:00"},
+    {id: 3, name: "JS Assist Two", start:"2016-11-14 00:00", end:"2016-11-21 00:00"},
   ]
 }
 
