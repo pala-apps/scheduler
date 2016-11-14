fd3290fe78c4f091f80fa3b00544c99121e35dab
Jay Chetty <jaypaulchetty@gmail.com>
2016-11-14 14:00:20 +0000

    display courses

--- web/static/js/components/AppContainer.jsx
+++ web/static/js/components/AppContainer.jsx
@@ -9,8 +9,19 @@ const AppContainer = ( { } ) => {
   ]
 
   const departments = [
-
+    { name: "Cohort 7",
+      periods: [1,1,1,1]
+    },
+    { name: "Cohort 8",
+      periods: [2,2,2,1]
+    }
   ]
+
+  const teams = {
+    1: { name:"Java" },
+    2: { name:"JS" }
+  }
+
   //
   // const periods = [
   //   {
@@ -20,9 +31,23 @@ const AppContainer = ( { } ) => {
   // ]
 
   const headers = periods.map( periodString => <div key={periodString}> { periodString } </div> )
+
+  const rows = departments.map( (department) =>{
+    const periodItems = department.periods.map( teamId => <div> { teams[teamId].name } </div> )
+    return(
+      <div className="row">
+        { periodItems }
+      </div>
+    )
+  })
+
+
   return(
-    <div className="l-flex">
-      { headers }
+    <div className="calendar">
+      <div className="row">
+        { headers }
+      </div>
+      { rows }
     </div>
   )
 }
