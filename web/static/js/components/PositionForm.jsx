f4210260a4ef09fbbf521d7e120ca04a2b26a04a
Richard Henry <r.henry1989@gmail.com>
2016-11-15 14:09:09 +0000

    form serializer working

--- web/static/js/components/PositionForm.jsx
+++ web/static/js/components/PositionForm.jsx
@@ -1,33 +1,10 @@
-// import { reduxForm, Field } from 'redux-form'
 import React from 'react'
-
-function formSerialize( formObjectChildren ) {
-  const data = {}
-
-  for ( let i = 0; i < formObjectChildren.length; i ++ ) {
-    const child = formObjectChildren[i];
-    const userInput = child.value;
-    const isNumber = child.type === "number";
-    const isCheckboxOrRadio = child.type === "checkbox" || child.type === "radio";
-    const isTextBasedInput = [ "text", "date", "month", "email", "datetime-local" ].some( ( type, index ) => { return type === child.type } )
-
-    if ( isNumber && userInput ) {
-      data[ child.name ] = Number( child.value );
-    }
-
-    if ( ( ( isCheckboxOrRadio ) && child.checked ) || ( isTextBasedInput && userInput ) ) {
-      data[child.name] = child.value
-    }
-
-  }
-
-  console.log( data );
-}
+import formSerializer from "../libs/formSerializer"
 
 const PositionForm = () => {
 
   return(
-    <form onSubmit={ (e) => { e.preventDefault(); console.dir( e.target.children ); formSerialize( e.target.children ) } }>
+    <form onSubmit={ (e) => { e.preventDefault(); console.dir( formSerializer( e.target.children ) ); } }>
       <input name="firstName" type="text" placeholder="First Name" />
       <input name="number" type="number" placeholder="Number" />
       <input name="email" type="email" placeholder="email" />
---
+++ web/static/js/libs/formSerializer.js
@@ -0,0 +1,26 @@
+export default function formSerializer( formObjectChildren ) {
+  const textInputs = [ "text", "date", "month", "email", "datetime-local" ];
+  const numberInputs = [ "number" ];
+  const checkedInputs = [ "checkbox", "radio" ]
+
+  const data = {}
+
+  for ( let i = 0; i < formObjectChildren.length; i ++ ) {
+    const child = formObjectChildren[i];
+    const userInput = child.value;
+    const isNumberInput = numberInputs.some( type => type === child.type );
+    const isCheckedInput = checkedInputs.some( type => type === child.type )
+    const isTextInput = textInputs.some( type => type === child.type )
+
+    if ( isNumberInput && userInput ) {
+      data[ child.name ] = Number( child.value );
+    }
+
+    if ( ( ( isCheckedInput ) && child.checked ) || ( isTextInput && userInput ) ) {
+      data[child.name] = child.value
+    }
+
+  }
+
+  return data;
+}
